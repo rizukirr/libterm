7986742adcda8c6586eeb8ed2a456ade66ca1525
rizukirr <rizkirr.xyz@gmail.com>
2026-05-05 20:49:23 +0700

    feat(utf8): implement lt__utf8_encode for 1-4 byte sequences
    Encode codepoints up to U+10FFFF, reject UTF-16 surrogates, and reject
    out-of-range values. Replaces the previous TODO stub.

--- src/shared/utf8.c
+++ src/shared/utf8.c
@@ -22,8 +22,34 @@ int lt__utf8_decode(const char *s, size_t len, lt_uchar *out) {
 }
 
 int lt__utf8_encode(lt_uchar ch, char out[4]) {
-  (void)ch;
-  (void)out;
-  /* TODO */
+  if (ch < 0x80) {
+    out[0] = (char)ch;
+    return 1;
+  }
+
+  if (ch < 0x800) {
+    out[0] = (char)(0xC0 | (ch >> 6));
+    out[1] = (char)(0x80 | (ch & 0x3F));
+    return 2;
+  }
+
+  if (ch < 0x10000) {
+    if (ch >= 0xD800 && ch <= 0xDFFF)
+      return 0;
+
+    out[0] = (char)(0xE0 | (ch >> 12));
+    out[1] = (char)(0x80 | ((ch >> 6) & 0x3F));
+    out[2] = (char)(0x80 | (ch & 0x3F));
+    return 3;
+  }
+
+  if (ch < 0x110000) {
+    out[0] = (char)(0xF0 | (ch >> 18));
+    out[1] = (char)(0x80 | ((ch >> 12) & 0x3F));
+    out[2] = (char)(0x80 | ((ch >> 6) & 0x3F));
+    out[3] = (char)(0x80 | (ch & 0x3F));
+    return 4;
+  }
+
   return 0;
 }
